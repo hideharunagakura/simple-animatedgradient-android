7565f06d04400ce55035fc1ab5dc3a7ed6333ad2
Hideharu Nagakura <nagakura@fout.jp>
2019-11-03 21:46:47 +0900

    add: Create an animated gradient background

--- app/src/main/java/com/example/apps/animatedgradient/MainActivity.kt
+++ app/src/main/java/com/example/apps/animatedgradient/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.apps.animatedgradient
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBackground()
+    }
+
+    private fun setupBackground() {
+        val container: ConstraintLayout = findViewById(R.id.main_layout)
+        val animationDrawable: AnimationDrawable = container.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
     }
 
 
